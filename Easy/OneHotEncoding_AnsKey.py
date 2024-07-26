234fee18b5c7174f71a328617d5195cada950090
Pranav Pawar <ppawar2612@gmail.com>
2024-07-26 23:28:24 +0530

    Update OneHotEncoding_AnsKey.py

--- Easy/OneHotEncoding_AnsKey.py
+++ Easy/OneHotEncoding_AnsKey.py
@@ -3,9 +3,12 @@ import numpy as np
 def to_categorical(x, n_col=None):
     if not n_col:
         n_col = np.amax(x) + 1
+    print(x.shape[0])
     one_hot = np.zeros((x.shape[0], n_col))
+    print(np.arange(x.shape[0]))
+    print(x)
     one_hot[np.arange(x.shape[0]), x] = 1
     return one_hot
 
-A = np.array([4,5,6])
+A = np.array([8,5,6])
 print(to_categorical(A))
