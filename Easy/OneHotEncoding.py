0ee5d381b4e1eb3d93fd8ce556bcb565ff42671d
Pranav Pawar <ppawar2612@gmail.com>
2024-07-26 23:03:31 +0530

    Update OneHotEncoding.py

--- Easy/OneHotEncoding.py
+++ Easy/OneHotEncoding.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def to_categorical(x,n_col=None):
-    array_result=np.array()
-    arrayi = np.array()
-    for i in range(x):
-        if i == x[i]:
-            arrayi[i]=1
-        else:
-            arrayi[i]=0
+def to_categorical(x, n_col=None):
+    array_result= []
+    classes=max(x)+1
+    arrayi = []
+    for i in x:
+        arrayi=[0]*classes
+        arrayi[i]=1
         array_result.append(arrayi)
     return array_result
-A = np.array([2,3,4])
+
+A = [2,3,4]
 print(to_categorical(A))
