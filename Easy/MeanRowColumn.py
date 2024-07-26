dda561017f1f8bd641410b3e647fe1753608831d
Pranav Pawar <ppawar2612@gmail.com>
2024-07-27 02:08:25 +0530

    Update MeanRowColumn.py

--- Easy/MeanRowColumn.py
+++ Easy/MeanRowColumn.py
@@ -1,2 +1,11 @@
+import numpy as np
+
 def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
+    if mode=='row':
+        means = np.mean(matrix, axis=1)
+    else:
+        means = np.mean(matrix, axis=0)
     return means
+
+K= [[1,2,3],[1,2,3],[1,2,3]]
+print(calculate_matrix_mean(K,'row'))
