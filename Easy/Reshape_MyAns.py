c475ae8cc81333eab9401bffd7fa1fe547bf63e5
Pranav Pawar <ppawar2612@gmail.com>
2024-07-27 23:09:12 +0530

    Update Reshape_MyAns.py

--- Easy/Reshape_MyAns.py
+++ Easy/Reshape_MyAns.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
-    #Write your code here and return a python list after reshaping by using numpy's tolist() method
-    return reshaped_matrix
+    a_new = np.array(a)
+    new_row = new_shape[0]
+    new_column = new_shape[1]
+    reshaped_matrix = a_new.reshape(new_row,new_column)
+    c = reshaped_matrix.tolist()
+    return c
